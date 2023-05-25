208715571550faf10ee58706e69cb1b37795d608
Malick <baye.gning@student-cs.fr>
2023-05-25 04:28:17 +0200

    update

--- unet.py
+++ unet.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate, BatchNormalization
+from keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate, BatchNormalization, Conv2DTranspose
 
 
 def unet_model(input_shape, filters_init=32):
@@ -31,28 +31,28 @@ def unet_model(input_shape, filters_init=32):
     drop5 = Dropout(0.5)(conv5)
 
     # Decoder
-    up6 = Conv2D(filters_init*8, 2, activation='relu',
-                 padding='same')(UpSampling2D(size=(2, 2))(drop5))
+    up6 = Conv2DTranspose(filters_init*8, 2, activation='relu',
+                          padding='same')(UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([drop4, up6], axis=3)
     conv6 = Conv2D(filters_init*8, 3, activation='relu',
                    padding='same')(merge6)
     conv6 = Conv2D(filters_init*8, 3, activation='relu', padding='same')(conv6)
 
-    up7 = Conv2D(filters_init*4, 2, activation='relu',
-                 padding='same')(UpSampling2D(size=(2, 2))(conv6))
+    up7 = Conv2DTranspose(filters_init*4, 2, activation='relu',
+                          padding='same')(UpSampling2D(size=(2, 2))(conv6))
     merge7 = concatenate([conv3, up7], axis=3)
     conv7 = Conv2D(filters_init*4, 3, activation='relu',
                    padding='same')(merge7)
     conv7 = Conv2D(filters_init*4, 3, activation='relu', padding='same')(conv7)
 
-    up8 = Conv2D(filters_init*2, 2, activation='relu',
-                 padding='same')(UpSampling2D(size=(2, 2))(conv7))
+    up8 = Conv2DTranspose(filters_init*2, 2, activation='relu',
+                          padding='same')(UpSampling2D(size=(2, 2))(conv7))
     merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(filters_init*2, 3, activation='relu',
                    padding='same')(merge8)
     conv8 = Conv2D(filters_init*2, 3, activation='relu', padding='same')(conv8)
 
-    up9 = Conv2D(filters_init, 2, activation='relu', padding='same')(
+    up9 = Conv2DTranspose(filters_init, 2, activation='relu', padding='same')(
         UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis=3)
     conv9 = Conv2D(filters_init, 3, activation='relu', padding='same')(merge9)
